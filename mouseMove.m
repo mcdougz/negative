8478ccc17d1adec822d64d61a21aed66639ed3e6
Shameen Azeez <shameen.azeez@googlemail.com>
2013-11-27 15:02:23 +0000

    can now get RGB on mouseMove

--- mouseMove.m
+++ mouseMove.m
@@ -1,13 +1,17 @@
 function mouseMove (object, eventdata)
     C = get (gca, 'CurrentPoint');
-    x = floor(num2str(C(1,1)));
-    y = floor(num2str(C(1,2)));
-    coords = strcat('(', x, ', ', y, ')');
+    x = fix(C(1,1));
+    y = fix(C(1,2));
+    coords = strcat('(', num2str(x), ', ', num2str(y), ')');
     set(findobj(gcf,'-depth',1,'Tag','txtCoords'),'String',coords);
     global IMG;
-    rgbinfo = strcat('[',size(IMG,1),',',size(IMG,2),']');
-    rgbinfo = strcat(rgbinfo,' (',x,',',y,')');
-    rgbinfo = strcat(rgbinfo,' R:',num2str(IMG(x,y,1)));
-    rgbinfo = strcat(rgbinfo,' G:',num2str(IMG(x,y,2)));
-    rgbinfo = strcat(rgbinfo,' B:',num2str(IMG(x,y,3)));
+    rgbinfo = strcat('[',num2str(size(IMG,2)),',',num2str(size(IMG,1)),']');
+    try
+        rgbinfo = strcat(rgbinfo,coords);
+        rgbinfo = strcat(rgbinfo,' R:',num2str(IMG(y,x,1)));
+        rgbinfo = strcat(rgbinfo,' G:',num2str(IMG(y,x,2)));
+        rgbinfo = strcat(rgbinfo,' B:',num2str(IMG(y,x,3)));
+    catch
+        rgbinfo = strcat(rgbinfo,' Click inside the image first!');
+    end %try
     set(findobj(gcf,'Tag','txtBottom'),'String',rgbinfo);
