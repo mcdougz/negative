4092cddc04894b0257776fd8118c3b18bf205ee9
Shameen Azeez <shameen.azeez@googlemail.com>
2014-02-05 17:01:11 +0000

    moved ColourSpaces

--- @ColourSpaces/ColourSpaces.m
+++ @ColourSpaces/ColourSpaces.m
@@ -2,25 +2,26 @@ classdef ColourSpaces
     % converts between different colour spaces. may switch to OOP
     properties
     end
-    methods
+    methods (Static = true)
         function y = normalize(val)
-            y = val/255.0;
+            y = double(val)/255.0;
         end
         function y = denormalize(val)
-            y = val*255;
+            y = double(val)*255.0;
         end
         % multiple return from stackoverflow.com/questions/4188139/matlab-multiple-return-values-from-a-function
         function [hue,sat,val] = RGBtoHSV(r,g,b)
+            % takes a normalized RGB and converts it into HSV
             % formula from rapidtables.com/convert/color/rgb-to-hsv.htm
-            r = normalize(r);
-            g = normalize(g);
-            b = normalize(b);
-            Cmax = max(r,g,b);
-            Cmin = min(r,g,b);
+            r = ColourSpaces.normalize(r);
+            g = ColourSpaces.normalize(g);
+            b = ColourSpaces.normalize(b);
+            Cmax = max([r,g,b]);
+            Cmin = min([r,g,b]);
             delta = Cmax-Cmin;
             %hue
             if (Cmax==r)
-                hue = 60*modp((g-b)/delta,6);
+                hue = 60*mod((g-b)/delta,6);
             elseif (Cmax==g)
                 hue = 60*(b-r)/delta+2;
             else
@@ -34,6 +35,10 @@ classdef ColourSpaces
             end
             %value
             val = Cmax;
+            
+            %hue = ColourSpaces.denormalize(hue);
+            %sat = ColourSpaces.denormalize(sat);
+            %val = ColourSpaces.denormalize(val);
         end
     end
 end
