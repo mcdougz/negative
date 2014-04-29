155c1bb46bce8556f12afc796c0d99fd5a84d794
Shameen Azeez <shameen.azeez@googlemail.com>
2014-04-29 02:38:08 +0100

    rgbStretchlim was overcontrasting because min/max were switched

--- rgbStretchlim.m
+++ rgbStretchlim.m
@@ -6,8 +6,8 @@ rstretch = stretchlim(y(:,:,1));
 gstretch = stretchlim(y(:,:,2));
 bstretch = stretchlim(y(:,:,3));
 
-low = max([rstretch(1),gstretch(1),bstretch(1)]);
-high = min([rstretch(2),gstretch(2),bstretch(2)]);
+low = min([rstretch(1),gstretch(1),bstretch(1)]);
+high = max([rstretch(2),gstretch(2),bstretch(2)]);
 
 y(:,:,1) = imadjust(y(:,:,1),[low high],[]);
 y(:,:,2) = imadjust(y(:,:,2),[low high],[]);
